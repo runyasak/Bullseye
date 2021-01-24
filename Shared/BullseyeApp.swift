8ed8512d9e9ac57ed0d3f0363c56b3fdb8061776
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2021-01-24 14:44:09 +0700

    Initial Commit

---
+++ Shared/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Shared
+//
+//  Created by Runyasak Chaengnaimuang on 24/1/2564 BE.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
