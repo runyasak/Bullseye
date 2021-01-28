0f58a3d18b2588238da23ae1e2bcb562be0f7039
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2021-01-29 00:38:38 +0700

    feat: implement rounded sliderValue

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ContentView: View {
 
     @State private var alertIsVisible: Bool = false
+    @State private var sliderValue: Double = 50.0
     
     var body: some View {
         VStack {
@@ -28,7 +29,7 @@ struct ContentView: View {
             HStack {
                 Text("1")
                     .bold()
-                Slider(value: .constant(50), in: 1.0...100.0)
+                Slider(value: self.$sliderValue, in: 1.0...100.0)
                 Text("100")
                     .bold()
             }
@@ -39,7 +40,7 @@ struct ContentView: View {
                 Text("Hit me")
             }
             .alert(isPresented: $alertIsVisible, content: {
-                return Alert(title: Text("Hello there"), message: Text("This is my first pop-up"), dismissButton: .default(Text("Awesome !")))
+                return Alert(title: Text("Hello there"), message: Text("The slider's value is \(Int(self.sliderValue.rounded()))."), dismissButton: .default(Text("Awesome !")))
             })
         }
     }
