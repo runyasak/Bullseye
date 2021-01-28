818810902c4a31aacfab6e5bd99027d51217d7c6
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2021-01-29 00:14:43 +0700

    feat: implement alertIsVisible and update project setting

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    @State private var alertIsVisible: Bool = false
+    
     var body: some View {
         VStack {
             VStack {
@@ -31,9 +34,13 @@ struct ContentView: View {
             }
             Button(action: {
                 print("Hello SwiftUI")
+                self.alertIsVisible = true
             }) {
                 Text("Hit me")
             }
+            .alert(isPresented: $alertIsVisible, content: {
+                return Alert(title: Text("Hello there"), message: Text("This is my first pop-up"), dismissButton: .default(Text("Awesome !")))
+            })
         }
     }
 }
