87dc62571cd5453427eae67a7256f09d689ade48
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2021-01-31 00:02:56 +0700

    refactor: remove unused return

--- Shared/Models/Game.swift
+++ Shared/Models/Game.swift
@@ -13,6 +13,6 @@ struct Game {
     var round: Int = 1
     
     func points(sliderValue: Int) -> Int {
-        return 100 - abs(target - sliderValue)
+        100 - abs(target - sliderValue)
     }
 }
